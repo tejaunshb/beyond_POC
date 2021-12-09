f285e5c9a7c0a895e75c5b1da27a8f85b9259680
Tejaunsh <tejaunsh@cavis.tech>
2021-12-09 12:46:01 +0530

    add overviewtable

--- src/Modal.js
+++ src/Modal.js
@@ -1,35 +1,52 @@
 import React from 'react'
+import 'antd/dist/antd.css';
 
+const { Table, Tag, Space } = require("antd");
+const { Column, ColumnGroup } = Table;
 export default function Modal(props) {
-	//var d = Object.values(JSON.parse(props.data))
-	//console.log(d)
+
 	var data = {}
 	data['ov'] = JSON.parse(props.data);
 	var overview = Object.keys(data.ov)
-	console.log(overview)
-	console.log(Object.values(data.ov))
+	var objArray = []
+
+	for (const key in data.ov) {
+		var obj = {}
+		obj['room'] = key
+		if (Object.hasOwnProperty.call(data.ov, key)) {
+			const element = data.ov[key];
+			for (const key1 in element) {
+
+				if (Object.hasOwnProperty.call(element, key1)) {
+					const elem = element[key1];
+
+					if (key1 == "fan") {
+						obj["fan"] = elem
+					}
+					if (key1 == "tv") {
+						obj["tv"] = elem
+					}
+					if (key1 == "light") {
+						obj["light"] = elem
+					}
+
+				}
+
+			}
+		}
+		objArray.push(obj)
+	}
+	console.log("objA", objArray)
 	return (
 		<div>
-			<p>Overview</p>
-			<table>
-				<thead>
-					<th>Area</th>
-					<th>Objects</th>
-					<th>qty</th>
-				</thead>
-				<tbody>
+			<h2>Overview</h2>
+			<Table dataSource={objArray}>
+				<Column title="Room" dataIndex="room" key="room" />
+				<Column title="Fan" dataIndex="fan" key="lastName" />
+				<Column title="Tv" dataIndex="tv" key="age" />
+				<Column title="Light" dataIndex="light" key="address" />
+			</Table>
 
-					{
-						//overview.map(item => {
-						//	return (
-						//		<tr>
-						//			{/*<td>{item.Bedroom1.fan}</td>*/}
-						//		</tr>
-						//	)
-						//})
-					}
-				</tbody>
-			</table>
 		</div>
 	)
 }
